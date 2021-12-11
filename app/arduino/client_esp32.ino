01a90463827d8351994bb0194d16263ca4df4cd7
Haytham Abuelfutuh <haytham@afutuh.com>
2021-12-11 13:58:09 -0800

    Add ESP21 equivalent code
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- app/arduino/client_esp32.ino
+++ app/arduino/client_esp32.ino
@@ -1,18 +1,19 @@
 
 #include "WiFi.h"
+#include <HTTPClient.h>
 
 //#include "arduino_secrets.h"
 ///////please enter your sensitive data in the Secret tab/arduino_secrets.h
-char ssid[] = "TheNetwork"; // your network SSID (name)
-char pass[] = "P@$$w0rd";   // your network password (use for WPA, or use as key for WEP)
-int keyIndex = 0;           // your network key Index number (needed only for WEP)
+const char *ssid = "TheNetwork"; // your network SSID (name)
+const char *pass = "P@$$w0rd";   // your network password (use for WPA, or use as key for WEP)
+int keyIndex = 0;                // your network key Index number (needed only for WEP)
 
 int status = WL_IDLE_STATUS;
 // if you don't want to use DNS (and reduce your sketch size)
 // use the numeric IP instead of the name for the server:
 //IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
-char server[] = "www.google.com"; // name address for Google (using DNS)
-const char userID[] = "1";
+const char *server = "www.google.com"; // name address for Google (using DNS)
+String userID = "1";
 
 // Initialize the Ethernet client library
 // with the IP address and port of the server
@@ -26,6 +27,20 @@ const int ledPin = 13;   // the number of the LED pin
 // variables will change:
 int buttonState = 0; // variable for reading the pushbutton status
 
+void initWiFi()
+{
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, pass);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.println(WiFi.localIP());
+}
+
 void setup()
 {
   // initialize the LED pin as an output:
@@ -33,21 +48,10 @@ void setup()
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
 
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED)
-  {
-    Serial.print("Attempting to connect to SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:
-    status = WiFi.begin(ssid, pass);
-    Serial.print("delaying 10 seconds");
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
-  Serial.println("Connected to wifi");
-  printWiFiStatus();
+  Serial.begin(115200);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
 }
 
 void loop()
@@ -71,37 +75,30 @@ void loop()
 
 void reportPoint()
 {
-  // if you get a connection, report back via serial:
-  if (client.connect(server, 8080))
-  {
-    Serial.println("connected to server");
-    // Make a HTTP request:
-    client.println("POST / HTTP/1.1");
+  WiFiClient client;
+  HTTPClient http;
 
-    client.print("Host: ");
-    client.println(server);
-    client.println("Connection: close");
+  // Your Domain name with URL path or IP address with path
+  http.begin(client, server);
 
-    client.print("id=");
-    client.println(userID);
-    client.println();
-  }
-}
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  // Data to send with HTTP POST
+  String httpRequestData = "id=" + userID;
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(httpRequestData);
 
-void printWiFiStatus()
-{
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+  /*
+      // If you need an HTTP request with a content type: application/json, use the following:
+      http.addHeader("Content-Type", "application/json");
+      // JSON data to send with HTTP POST
+      String httpRequestData = "{\"api_key\":\"" + apiKey + "\",\"field1\":\"" + String(random(40)) + "\"}";           
+      // Send HTTP POST request
+      int httpResponseCode = http.POST(httpRequestData);*/
 
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  Serial.print("HTTP Response code: ");
+  Serial.println(httpResponseCode);
 
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.print(rssi);
-  Serial.println(" dBm");
+  // Free resources
+  http.end();
 }
